0d73f0bdbb3fa19bf1c563f0fa4f7c4040a7268d
akshay <akshayjha2111@gmail.com>
2025-09-14 11:14:46 +0530

    files uploading

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@
 
 import express from 'express';
 import moderationRoutes from './routes/moderation.routes.js';
-
+import cors from 'cors';
 const app = express();
-
 app.use(express.json());
+app.use(cors())
 
 // Mount moderation API routes under /moderation
 app.use('/moderation', moderationRoutes);
